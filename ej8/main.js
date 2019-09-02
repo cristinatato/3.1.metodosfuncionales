96d35f98d6c06b2a128ed8fccf0d5501342714bc
cristinatato <c.tatodelatorre@gmail.com>
2019-09-02 09:52:24 +0200

    add ejercicio 8

---
+++ ej8/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const runners = [
+    { name: 'Gregory Goyle', time: 56 },
+    { name: 'Nymphadora Tonks', time: 9 },
+    { name: 'Luna Lovegood', time: 45 },
+    { name: 'Cedric Diggory', time: 28 },
+    { name: 'Cho Chang', time: 35 },
+  ];
+
+const winner = runners.reduce((acc, runner) => {
+    console.log('acc', acc);
+    console.log(runner);
+    if (acc.time <= runner.time) {
+        return acc;
+    } else {
+        return runner;
+    }
+})
+console.log(winner)
